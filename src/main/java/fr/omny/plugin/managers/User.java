479d9343b9decd07abfbba397cf7e14349637d54
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-01 09:51:56 +0100

    Redisson annotations

--- src/main/java/fr/omny/plugin/managers/User.java
+++ src/main/java/fr/omny/plugin/managers/User.java
@@ -4,12 +4,16 @@ package fr.omny.plugin.managers;
 import java.util.UUID;
 
 import org.bukkit.entity.Player;
+import org.redisson.api.annotation.REntity;
+import org.redisson.api.annotation.RId;
 
 import lombok.Getter;
 
 @Getter
+@REntity
 public class User {
 
+	@RId
 	private UUID id;
 	private int token;
 	private String data;
