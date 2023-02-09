4a8ace630fa736070efbcf8198ad1817ccd3ea7b
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-09 22:48:04 +0100

    use mongo repo instead of redis

--- src/main/java/fr/omny/plugin/managers/UserRepository.java
+++ src/main/java/fr/omny/plugin/managers/UserRepository.java
@@ -5,11 +5,11 @@ import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
-import fr.omny.flow.data.RedisRepository;
+import fr.omny.flow.data.MongoRepository;
 import fr.omny.flow.data.Repository;
 
 @Repository
-public interface UserRepository extends RedisRepository<User, UUID>{
+public interface UserRepository extends MongoRepository<User, UUID>{
 	
 	default Optional<User> get(Player player){
 		return findById(player.getUniqueId());
